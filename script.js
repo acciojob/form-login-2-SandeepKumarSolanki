e767402d9caa98cc7b659bc7a59441d1a74859ed
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-17 10:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 //your JS code here. If required.
 
 let form = document.querySelector('form');
-let nameBox = document.getElementById('name');
+let nameBox = document.getElementById('firstname');
 let lastNameBox = document.getElementById('lastname');
 let phoneBox = document.getElementById('tel');
 let emailBox = document.getElementById('email');
 
-form.addEventListener("submit" , (e)=> {
+form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	let name = nameBox.value;
-	let lastName = lastNameBox.value;
-	let tel = phoneBox.value;
-	let email = emailBox.value;
 
-	if(name && lastName && tel && email){
-		alert(`FirstName:${name} LastName: ${lastName} Phone Number: ${tel} Email ID:${email}`)
+	let name = nameBox.value.trim();
+	let lastName = lastNameBox.value.trim();
+	let tel = phoneBox.value.trim();
+	let email = emailBox.value.trim();
+
+	if (name && lastName && tel && email) {
+		alert(`FirstName: ${name} LastName: ${lastName} Phone Number: ${tel} Email ID: ${email}`);
 	}
-})
+});
