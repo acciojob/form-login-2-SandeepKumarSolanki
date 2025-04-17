b96698360a1ebbc94f3c7c18c0f50005baf46c10
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-17 10:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+let form = document.querySelector('form');
+let nameBox = document.getElementById('name');
+let lastNameBox = document.getElementById('lastname');
+let phoneBox = document.getElementById('tel');
+let emailBox = document.getElementById('email');
+
+form.addEventListener("submit" , (e)=> {
+	e.preventDefault();
+	let name = nameBox.value;
+	let lastName = lastNameBox.value;
+	let tel = phoneBox.value;
+	let email = emailBox.value;
+
+	alert(`FirstName:${name} LastName: ${lastName} Phone Number: ${tel} Email ID:${email}`)
+})
